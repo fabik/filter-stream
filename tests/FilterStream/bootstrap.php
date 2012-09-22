6acc66a65719ec5febd4e069e228026635e619ee
Jan-Sebastian Fabik <honza@fabik.org>
2012-09-22 17:15:09 +0200

    Added tests bootstrap.

---
+++ tests/FilterStream/bootstrap.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Test initialization and helpers.
+ *
+ * @author     Jan-Sebastian Fabik
+ * @package    FilterStream\Test
+ */
+
+
+require __DIR__ . '/../../vendor/nette/tester/Tester/bootstrap.php';
+require __DIR__ . '/../../vendor/nette/nette/Nette/loader.php';
+require __DIR__ . '/../../FilterStream/loader.php';
+
+
+// configure environment
+date_default_timezone_set('Europe/Prague');
+
+
+// temporary directory garbage collection
+if (lcg_value() < 0.01) {
+	foreach (glob(__DIR__ . '/../tmp/*[0-9]', GLOB_ONLYDIR) as $dir) {
+		if (time() - @filemtime($dir) > 300 && @rename($dir, $dir . '-delete')) {
+			TestHelpers::purge($dir . '-delete');
+			rmdir($dir . '-delete');
+		}
+	}
+}
+
+// create temporary directory
+define('TEMP_DIR', __DIR__ . '/../tmp/' . getmypid());
+TestHelpers::purge(TEMP_DIR);
+
+
+$_SERVER = array_intersect_key($_SERVER, array_flip(array('PHP_SELF', 'SCRIPT_NAME', 'SERVER_ADDR', 'SERVER_SOFTWARE', 'HTTP_HOST', 'DOCUMENT_ROOT', 'OS', 'argc', 'argv')));
+$_SERVER['REQUEST_TIME'] = 1234567890;
+$_ENV = $_GET = $_POST = array();
+
+if (PHP_SAPI !== 'cli') {
+	header('Content-Type: text/plain; charset=utf-8');
+}
+
+
+if (extension_loaded('xdebug')) {
+	xdebug_disable();
+	TestHelpers::startCodeCoverage(__DIR__ . '/coverage.dat');
+}
+
+
+function id($val) {
+	return $val;
+}
