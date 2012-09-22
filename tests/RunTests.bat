7540cbd3951072a9fd2670b09c7294c1e77fc3d2
Jan-Sebastian Fabik <honza@fabik.org>
2012-09-22 17:09:09 +0200

    Added tests runner.

---
+++ tests/RunTests.bat
@@ -0,0 +1,1 @@
+@php.exe "%~dp0..\vendor\nette\tester\Tester\RunTests.phpc" -p php-cgi.exe -c "%~dp0php.ini-win" -j 20 -log "%~dp0test.log" %*
---
+++ tests/run-tests.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# saves the path to this script's directory
+dir=`dirname $0`
+
+# absolutizes the path if necessary
+if echo "$dir" | grep -v ^/ > /dev/null; then
+	dir="`pwd`/$dir"
+fi
+
+PhpIni=
+while getopts ":c:" opt; do
+	case $opt in
+	c)	PhpIni="$OPTARG"
+		;;
+
+	:)	echo "Missing argument for -$OPTARG option"
+		exit 2
+		;;
+	esac
+done
+
+# runs RunTests.php with script's arguments, add default php.ini if not specified
+if [ -n "$PhpIni" ]; then
+	php -c "$PhpIni" "$dir/../vendor/nette/tester/Tester/RunTests.phpc" "$@"
+else
+	php -c "$dir/php.ini-unix" "$dir/../vendor/nette/tester/Tester/RunTests.phpc" -c "$dir/php.ini-unix" "$@"
+fi
+error=$?
+
+# print *.actual on the output
+if [ $error -ne 0 ]; then
+	for i in $(find . -name \*.actual); do echo "--- $i"; cat $i; echo; echo; done
+	exit $error
+fi
